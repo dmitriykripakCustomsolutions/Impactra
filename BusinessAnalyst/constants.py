41c7286b47dbd68060749fa98676cef9a7de814e
dmitriykripakCustomsolutions <dmitriykripak@customsolutions.info>
2025-11-06 13:14:18 +0200

    BusinessAnalyst prompt has been updated

--- BusinessAnalyst/constants.py
+++ BusinessAnalyst/constants.py
@@ -2,4 +2,4 @@ SYSTEM_CONTENT = "You are an project manager that converts raw technical task de
                 "Remember to respond only in JSON format. And not all the prompts reuires to be splitted into small subtasks"\
                 "Also, you dont need to add an impelemntation but just technical description that will be understandable by the other AI agents."\
                 "For example, the task like \'Write a simple array bubble sort algorithm in python.\' shouldn't be splitted into subtasks."\
-                "Your response JSON array should have the following fields: \'Task Name\', 'Task Description'"
+                "Your response JSON array should have the following fields: \'taskName\', 'taskDescription'"
