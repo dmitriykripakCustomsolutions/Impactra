51fa9fb43d6de6570efdaa775a3e88e4d2069c20
dmitriykripakCustomsolutions <dmitriykripak@customsolutions.info>
2025-11-06 14:01:31 +0200

    n8n workflow file has been updated

--- CodeRunner/main.py
+++ CodeRunner/main.py
@@ -5,15 +5,7 @@ app = Flask(__name__)
 
 @app.route('/run-code', methods=['POST'])
 def receive_message():
-    app.logger.info(f"\'run-code\' Endpoint works")
-    # if request.is_json:
-    #     data = request.get_json()
-    #     message = data.get('message', '')
-    # else:
-    #     # If form-urlencoded
-    #     message = request.form.get('message', '')
-    # app.logger.info(f"A message received: {message}")
-    # return jsonify({"status": "ok", "received": message}), 200
+    # "completionResult": result
     return jsonify({"message": "\'run-code\' Endpoint works"})
 
 
